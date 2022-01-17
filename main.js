325868d76e896fa39e673d4d433c5be1aedca892
Alexander Burgos <77622127+alexanderuk82@users.noreply.github.com>
2022-01-17 04:43:08 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 "use strict";
 
 // Initializing the owl carrousel
+
 $(".owl-carousel").owlCarousel({
   stagePadding: 30,
   loop: true,
@@ -18,3 +19,36 @@ $(".owl-carousel").owlCarousel({
     },
   },
 });
+
+//Initialization of the counter counter
+const countdown = () => {
+  //!Define the final date
+
+  const date = new Date("AUG 27, 2022 00:00:00").getTime();
+
+  const now = new Date().getTime();
+  const gap = date - now;
+
+  //!Define the variables
+
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  //!Calculation of the time
+
+  const textday = Math.floor(gap / day);
+  const texthour = Math.floor((gap % day) / hour);
+  const textminute = Math.floor((gap % hour) / minute);
+  const textsecond = Math.floor((gap % minute) / second);
+
+  //!iNSERT values on DOM elements
+
+  document.querySelector(".day").innerText = textday;
+  document.querySelector(".hour").innerText = texthour;
+  document.querySelector(".minute").innerText = textminute;
+  document.querySelector(".second").innerText = textsecond;
+};
+
+setInterval(countdown, 1000);
